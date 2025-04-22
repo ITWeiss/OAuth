2c79a71b8e1306e9e3db48bb04ebd4f2b900f72c
ITWeiss <gottfried98@bk.ru>
2025-04-22 21:07:07 +0700

    Обеспечение безопасности REST-приложения - настройка CORS через Spring Security - создан ssl-сертификата с помощью keytool - добавлен браузерный клиент на другом порту http://localhost:3000

--- src/main/java/com/example/oauth/OAuthApplication.java
+++ src/main/java/com/example/oauth/OAuthApplication.java
@@ -3,11 +3,16 @@ package com.example.oauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Класс для запуска приложения
+ *
+ * @author ITWeiss
+ */
 @SpringBootApplication
 public class OAuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OAuthApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(OAuthApplication.class, args);
+  }
 
 }
